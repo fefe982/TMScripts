82a8277a2d15a4483b5b7561b6a61df94e6c760f
Yongxin Wang <fefe.wyx@gmail.com>
2016-04-09 20:13:55 +0800

    add dream nine

---
+++ sppfdream.js
@@ -0,0 +1,12 @@
+var actions = [
+    [/main%2Farena%2Fmain%2Fmatch_result_flash%3F/, 'flashJT', '#main_container'],
+    [/main%2Farena%2Fmain%2Fselect_tactics%3F/, 'formJ', 'form[action*="main%2Farena%2Fmain%2Fplayball_exe%3F"]'],
+    [/main%2Farena%2Fmain%3F/, 'aJ', '#arena_back > div.arena_btn_only > a'],
+    [/main%2Fgacha%2Fmain%2F%3Faction_eventgacha/, 'formJ', '#shortCut'], 
+    [/main%2Fpresent%2Freceive%2Fmain%2Fgacha_result/, 'aJ', '#shortCut'],
+    [/main%2Freinforce%2Fmain%2Findex%2F/, 'aJ', 'a[href*="main%2Freinforce%2Fmain%2Frecommendexe"]'],
+    //[/main%2Freinforce%2Fmain%2Fwith_item%3F/, 'aJ', 'a[href*="main%2Freinforce%2Fmain%2Frecommendexe"]'],
+    [/main%2Freinforce%2Fmain%2Fitem_use_confirm/, 'formJ', 'form[action*="main%2Freinforce%2Fmain%2Fitem_use_execute"]'],
+    [/%2Fflash%2F/, 'flashJT', '#tween_b_root'],
+    [/XXXXXXXXXXXXX/]
+];
