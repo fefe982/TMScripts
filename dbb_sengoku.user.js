dbbb8b1a5ae55ac08d7a2516eb221ad4a8b550f7
Yongxin Wang <fefe.wyx@gmail.com>
2016-10-02 22:49:26 +0800

    [sengoku] bug fix, & scroll [world] reformat, game entry

--- dbb_sengoku.user.js
+++ dbb_sengoku.user.js
@@ -5,6 +5,7 @@
 // @description  try to take over the world!
 // @author       You
 // @match        http://s01.d-bb.com/cgi-bin/*
+// @noframes
 // @grant        GM_log
 // ==/UserScript==
 
@@ -16,7 +17,7 @@
             return key === "" ? value : decodeURIComponent(value);
         }) : {};
     GM_log(path);
-
+    GM_log(param);
     // Returns a random integer between min (included) and max (included)
     // Using Math.round() will give you a non-uniform distribution!
     function getRandomIntInclusive(min, max) {
@@ -27,18 +28,18 @@
 
     switch (path) {
     case 'sdb_into_battle_event.cgi':
-        var links = $('#main > div > div > ul > li > a:has(img[src*="quest_icon_sensyu.png"])');
-        if (links.length !== 0) {
-            links[getRandomIntInclusive(0, links.length - 1)].click();
-        }
-        break;
-    case 'sdb_into_battle_event.cgi':
-        if (param.stage === 2) {
+        if (param.team_select === 'on') {
+            $('#main > form > p > input[type="image"]')[0].click();
+        } else if (param.stage === '2') {
             $('#main > div > div > p.syutujin > a')[0].click();
         } else if (param.stage !== undefined) {
             $('#main > div > div > p.levelbtns > a:nth-child(2)')[0].click();
-        } else if (param.team_select === 'on') {
-            $('#main > form > p > input[type="image"]')[0].click();
+        } else {
+            //GM_log(Object.getOwnPropertyNames(param));
+            var links = $('#main > div > div > ul > li > a:has(img[src*="quest_icon_sensyu.png"])');
+            if (links.length !== 0) {
+                links[getRandomIntInclusive(0, links.length - 1)].click();
+            }
         }
         break;
     case 'sdb_into_battle_another.cgi':
@@ -46,7 +47,9 @@
         break;
     case 'sdb_npc_game_play.fcgi':
         GM_log($('#wrap > div.btn_play_retry > a[href*="surrender=on"]').length > 0);
-        if ($('#finish').length > 0) {
+        if (param.result === 'on') {
+            $('#wrap > div.mark > div.lastbtn > a')[0].click();
+        } else if ($('#finish').length > 0) {
             $('#finish')[0].click();
         } else if ($('#wrap > div.btn_play_retry > a[href*="surrender=on"]').length > 0) {
             GM_log("XXXXXXXXX");
@@ -58,21 +61,21 @@
             $('#wrap > div.btn_back2top > a')[0].click();
         }
         break;
-    case 'sdb_npc_game_play.fcgi':
-        GM_log($('#wrap > div.mark > div.lastbtn > a'));
-        $('#wrap > div.mark > div.lastbtn > a')[0].click();
-        break;
     case 'sdb_castle.cgi':
         if ($('#main > div > div > div > ul > li.register > a').length > 0) {
             $('#main > div > div > div > ul > li.register > a')[0].click();
         }
         break;
     case 'sdb_trader.cgi':
+        if ($('#main > div > div.akindo_select > div > p.redo > a').length > 0) {
+            $('#main > div > div.akindo_select > div > p.redo > a')[0].click();
+            break;
+        }
         var wood = +$('#wood').text();
         var leather = +$('#hide').text();
         var iron = +$('#iron').text();
         if ($('#main > div > form').length === 0) {
-            return;
+            break;
         }
         if (wood <= leather && wood <= iron) {
             $('#main > div > form > div > div > ul:nth-child(2) > li.kinki > label')[0].click();
@@ -83,6 +86,23 @@
         }
         $('#main > div > form > div > div > ul:nth-child(4) > li.two_houres > label')[0].click();
         $('#disp > input[type="image"]:nth-child(2)').attr('onclick', 'return true').click();
+        break;
+    case 'sdb_artisan.cgi':
+        //shokunin_dougubox
+        if ($('.shokunin_makimonobox').length === 0) {
+            $('#main > div.menu.shokunin_menu > ul.shokunin_mainmenu > li:nth-child(3) > a')[0].click();
+        }
+        break;
+    case 'sdb_create_scroll.cgi':
+        if (param.detail === "1") {
+            $('#main > div.shokunin_seisan > div > form > p.item_buttonbox > input[type="image"]').click();
+        } else if ($('#main > div.shokunin_seisan > ul.shokunin_categorylist > li').length > 0) {
+            $('#main > div.shokunin_seisan > ul.shokunin_categorylist > li:nth-child(1) > div > p.item_dougu_button > a')[0].click();
+        } else {
+            if ($('#main > div.shokunin_seisan > ul.shokunin_categorybox > li:has(a.category_on) + li').length > 0) {
+                $('#main > div.shokunin_seisan > ul.shokunin_categorybox > li:has(a.category_on) + li > a')[0].click();
+            }
+        }
         break;
     }
     // Your code here...
--- dbb_world.user.js
+++ dbb_world.user.js
@@ -9,102 +9,112 @@
 // @grant        GM_log
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
     var path = location.pathname.split('/').pop();
     var param = location.search.substring(1);
-    param = param?JSON.parse('{"' + param.replace(/^&/, '').replace(/&/g, '","').replace(/=/g,'":"') + '"}', function(key, value) { return key===""?value:decodeURIComponent(value); }):{};
+    param = param ? JSON.parse('{"' + param.replace(/^&/, '').replace(/&/g, '","').replace(/=/g, '":"') + '"}', function (key, value) {
+            return key === "" ? value : decodeURIComponent(value);
+        }) : {};
     GM_log(path);
     GM_log(param);
     switch (path) {
-        case 'dbb_card_gacha.cgi':
-            // gacha, get next card
-            var gacha = jQuery('#retrial > a');
-            GM_log(gacha);
-            setTimeout(function () {jQuery('#retrial > a').attr('onclick', 'return true;')[0].click();}, 1000);
+    case 'dbb_card_gacha.cgi':
+        // gacha, get next card
+        var gacha = jQuery('#retrial > a');
+        GM_log(gacha);
+        setTimeout(function () {
+            jQuery('#retrial > a').attr('onclick', 'return true;')[0].click();
+        }, 1000);
+        break;
+    case 'dbb_level_up.cgi':
+        var grade = jQuery('#levelup_gr').text().match(/\d+/);
+        if (grade)
+            grade = +grade[0];
+        var level = jQuery('#levelup_levv').text().match(/\d+/);
+        if (level)
+            level = +level[0];
+        GM_log(grade);
+        GM_log(level);
+        if (grade !== 1)
             break;
-        case 'dbb_level_up.cgi':
-            var grade = jQuery('#levelup_gr').text().match(/\d+/);
-            if (grade) grade = +grade[0];
-            var level = jQuery('#levelup_levv').text().match(/\d+/);
-            if (level) level = +level[0];
-            GM_log(grade);
-            GM_log(level);
-            if (grade !== 1) break;
-            var card_type = jQuery('#card_type').text();
-            GM_log(card_type);
-            var add_time = (level-1) * 3;
-            var halt = false;
-            for (var i = 1; i <= 8; ++i) {
-                var row = jQuery('#data' + i + '_row');
-                var add_five_button = row.find('td:nth-child(5) > input[type="button"]');
-                GM_log(add_five_button);
-                var bonus = +add_five_button[0].getAttribute('onclick').split(',')[4];
-                GM_log(bonus);
-                var current = +row.find('td:nth-child(9)').text();
-                var add_row = (Math.floor(add_time / 8) + (i <= add_time % 8 ? 1 : 0)) * 5;
-                GM_log(add_row);
-                if (bonus > 0) {
-                    add_row += Math.floor(add_row / bonus);
-                }
-                GM_log(add_row);
-                if (current < add_row) {
-                    add_five_button.click();
-                } else if (current > add_row) {
-                    halt = true;
-                }
+        var card_type = jQuery('#card_type').text();
+        GM_log(card_type);
+        var add_time = (level - 1) * 3;
+        var halt = false;
+        for (var i = 1; i <= 8; ++i) {
+            var row = jQuery('#data' + i + '_row');
+            var add_five_button = row.find('td:nth-child(5) > input[type="button"]');
+            GM_log(add_five_button);
+            var bonus = +add_five_button[0].getAttribute('onclick').split(',')[4];
+            GM_log(bonus);
+            var current = +row.find('td:nth-child(9)').text();
+            var add_row = (Math.floor(add_time / 8) + (i <= add_time % 8 ? 1 : 0)) * 5;
+            GM_log(add_row);
+            if (bonus > 0) {
+                add_row += Math.floor(add_row / bonus);
             }
-            if (!halt) {
-                jQuery('#levelup_btnbox > ul > li.last > input').attr('onclick', 'return true;').click();
+            GM_log(add_row);
+            if (current < add_row) {
+                add_five_button.click();
+            } else if (current > add_row) {
+                halt = true;
             }
-            break;
-        case 'dbb_regist_team_player.fcgi':
-        case 'dbb_regist_real_team_player.cgi':
-            var lvup = jQuery('td.lvup > a:first');
-            if (lvup.length > 0) {
-                lvup[0].click();
-            }
-            break;
-        case 'dbb_card_list.cgi':
-            GM_log("in");
-            lvup = jQuery('a[href*="dbb_level_up.cgi"]');
-            GM_log(lvup);
-            if (lvup.length > 0) {
-                lvup[0].click();
-            }
-            break;
-        case 'dbb_training.cgi':
-            if (!('detail' in param)) {
-                if (jQuery('#downchoice > ul > li:nth-child(6) > select > option:contains("レベル")').attr('selected') === true &&
-                    jQuery('#downchoice > ul > li:nth-child(7) > select > option:contains("降順")').attr('selected') === true) {
-                    GM_log("good");
-                    jQuery('#shoji_cardarea > div.card_tr > div.cardwaku').each(function () {
-                        GM_log("hi");
-                        var grade = +jQuery(this).find('div.senshucard > div > div > div > span.card_grade > img').attr("src").match(/grade(\d)/)[1];
-                        var lvl = +jQuery(this).find('div.senshucard > div > div > div > span.card_level').text();
-                        GM_log(grade);
-                        GM_log(lvl);
-                        if (grade === 1 && lvl < 30) {
-                            jQuery(this).find('ul > li > a')[0].click();
-                            GM_log(jQuery(this).find('ul > li > a'));
-                            return false;
-                        }
-                        //jQuery('#shoji_cardarea > div.card_tr > div > ul > li > a').click();
-                    });
-                } else {
-                    jQuery('#downchoice > ul > li:nth-child(6) > select > option:contains("レベル")').attr('selected', true);
-                    jQuery('#downchoice > ul > li:nth-child(7) > select > option:contains("降順")').attr('selected', true);
-                    jQuery('#downchoice > ul > li:nth-child(8) > input').click();
-                }
+        }
+        if (!halt) {
+            jQuery('#levelup_btnbox > ul > li.last > input').attr('onclick', 'return true;').click();
+        }
+        break;
+    case 'dbb_regist_team_player.fcgi':
+    case 'dbb_regist_real_team_player.cgi':
+        var lvup = jQuery('td.lvup > a:first');
+        if (lvup.length > 0) {
+            lvup[0].click();
+        }
+        break;
+    case 'dbb_card_list.cgi':
+        GM_log("in");
+        lvup = jQuery('a[href*="dbb_level_up.cgi"]');
+        GM_log(lvup);
+        if (lvup.length > 0) {
+            lvup[0].click();
+        }
+        break;
+    case 'dbb_training.cgi':
+        if (!('detail' in param)) {
+            if (jQuery('#downchoice > ul > li:nth-child(6) > select > option:contains("レベル")').attr('selected') === true &&
+                jQuery('#downchoice > ul > li:nth-child(7) > select > option:contains("降順")').attr('selected') === true) {
+                GM_log("good");
+                jQuery('#shoji_cardarea > div.card_tr > div.cardwaku').each(function () {
+                    GM_log("hi");
+                    var grade = +jQuery(this).find('div.senshucard > div > div > div > span.card_grade > img').attr("src").match(/grade(\d)/)[1];
+                    var lvl = +jQuery(this).find('div.senshucard > div > div > div > span.card_level').text();
+                    GM_log(grade);
+                    GM_log(lvl);
+                    if (grade === 1 && lvl < 30) {
+                        jQuery(this).find('ul > li > a')[0].click();
+                        GM_log(jQuery(this).find('ul > li > a'));
+                        return false;
+                    }
+                    //jQuery('#shoji_cardarea > div.card_tr > div > ul > li > a').click();
+                });
             } else {
-                jQuery('#park_contents > div:nth-child(2) > div > table > tbody > tr > th > input[type="radio"]:last').attr('checked', 'true');
-                jQuery('#park_contents > div:nth-child(2) > p > input').attr('onclick', 'return true').click();
+                jQuery('#downchoice > ul > li:nth-child(6) > select > option:contains("レベル")').attr('selected', true);
+                jQuery('#downchoice > ul > li:nth-child(7) > select > option:contains("降順")').attr('selected', true);
+                jQuery('#downchoice > ul > li:nth-child(8) > input').click();
             }
-            break;
-        case 'dbb_spa.cgi':
-            if (jQuery('#sasikae_cardarea > div > div > div:nth-child(1) > ul > li > a').length > 0) {
-                jQuery('#sasikae_cardarea > div > div > div:nth-child(1) > ul > li > a').attr('onclick', 'return true')[0].click();
-            }
-            break;
+        } else {
+            jQuery('#park_contents > div:nth-child(2) > div > table > tbody > tr > th > input[type="radio"]:last').attr('checked', 'true');
+            jQuery('#park_contents > div:nth-child(2) > p > input').attr('onclick', 'return true').click();
+        }
+        break;
+    case 'dbb_spa.cgi':
+        if (jQuery('#sasikae_cardarea > div > div > div:nth-child(1) > ul > li > a').length > 0) {
+            jQuery('#sasikae_cardarea > div > div > div:nth-child(1) > ul > li > a').attr('onclick', 'return true')[0].click();
+        }
+        break;
+    case 'dbb_road_game_entry_normal.fcgi':
+        jQuery('#entrygame_cenbox02 > div > p:nth-child(2) > a')[0].click();
+        break;
     }
 })();
