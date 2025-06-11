d360a823a325580fc4828912754bd17be6e1cc59
Yongxin Wang <fefe82@qq.com>
2025-06-11 10:23:34 +0800

    update mlb

--- mlb.js
+++ mlb.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         mlb
 // @namespace    http://tampermonkey.net/
-// @version      2024-08-08
+// @version      2025-06-11
 // @description  try to take over the world!
 // @author       Yongxin Wang
 // @match        https://www.mlb.com/gameday/*
@@ -13,7 +13,7 @@
     "use strict";
     console.log("tampermonkey");
     function replaceContent(root) {
-        let children = root.getElementsByClassName("ptNqa");
+        let children = root.querySelectorAll(".ptNqa");
         for (let p of children) {
             console.log(p);
             console.log(p.childNodes[0]);
@@ -26,6 +26,24 @@
             }
             let kmph = (mph * 1.609344).toFixed(2);
             p.textContent = p.textContent + " / " + kmph + " kmph";
+        }
+        children = root.querySelectorAll(".data-point");
+        for (let p of children) {
+            console.log(p);
+            console.log(p.childNodes[0]);
+            if (p.childNodes.length != 1) {
+                continue;
+            }
+            let m = p.childNodes[0].textContent.match(/^([\d.]+) mph$/);
+            if (!m) {
+                continue;
+            }
+            let mph = parseFloat(m[1]);
+            if (!mph) {
+                continue;
+            }
+            let kmph = (mph * 1.609344).toFixed(2);
+            p.textContent = kmph + " kmph";
         }
     }
     replaceContent(document.body);
