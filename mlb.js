7c2597574f36bf8e91020d9bddb845cc99ab4168
Yongxin Wang <fefe82@qq.com>
2025-06-21 10:35:03 +0800

    mod environment

--- mlb.js
+++ mlb.js
@@ -12,7 +12,9 @@
 (function () {
   "use strict";
   console.log("tampermonkey");
-  function replaceContent(root) {
+  let mph_to_kmph = (mph) => (mph * 1.609344).toFixed(2);
+  let mph_to_mps = (mph) => (mph * 0.447037).toFixed(1);
+  function replaceBallSpeed(root) {
     let children = root.querySelectorAll(".ptNqa");
     for (let p of children) {
       console.log(p);
@@ -24,13 +26,13 @@
       if (!mph) {
         continue;
       }
-      let kmph = (mph * 1.609344).toFixed(2);
+      let kmph = mph_to_kmph(mph);
       p.textContent = p.textContent + " / " + kmph + " kmph";
     }
-    children = root.querySelectorAll(".data-point");
+  }
+  function replacePitchSpeed(root) {
+    let children = root.querySelectorAll(".data-point");
     for (let p of children) {
-      console.log(p);
-      console.log(p.childNodes[0]);
       if (p.childNodes.length != 1) {
         continue;
       }
@@ -42,9 +44,41 @@
       if (!mph) {
         continue;
       }
-      let kmph = (mph * 1.609344).toFixed(2);
+      let kmph = mph_to_kmph(mph);
       p.textContent = kmph + " kmph";
     }
+  }
+  function replaceEnvironment(root) {
+    let children = root.querySelectorAll(".fazIkQ");
+    for (let p of children) {
+      if (p.attributes["__tag"]) {
+        continue;
+      }
+      p.attributes["__tag"] = "1";
+      if (p.childNodes[0].textContent == "Weather") {
+        let m = p.childNodes[2].textContent.match(/^(\d+) degrees(.*)$/);
+        if (!m) {
+          continue;
+        }
+        let deg = ((parseInt(m[1]) - 32) / 1.8).toFixed(1);
+        p.childNodes[2].textContent = deg + " ℃" + m[2];
+      }
+      if (p.childNodes[0].textContent == "Wind") {
+        console.log(p.childNodes[2].textContent);
+        let m = p.childNodes[2].textContent.match(/^(\d+) mph(.*)$/);
+        if (!m) {
+          continue;
+        }
+        let mph = parseFloat(m[1]);
+        let mps = mph_to_mps(mph);
+        p.childNodes[2].textContent = mps + " m/s" + m[2];
+      }
+    }
+  }
+  function replaceContent(root) {
+    replaceBallSpeed(root);
+    replacePitchSpeed(root);
+    replaceEnvironment(root);
   }
   replaceContent(document.body);
   const observer = new MutationObserver((mutations) => {
