c88398622b499ca3e87a8fd8f6eb9b06a4518e2a
Yongxin Wang <fefe82@qq.com>
2025-06-21 10:08:02 +0800

    format

--- mlb.js
+++ mlb.js
@@ -10,55 +10,55 @@
 // ==/UserScript==
 
 (function () {
-    "use strict";
-    console.log("tampermonkey");
-    function replaceContent(root) {
-        let children = root.querySelectorAll(".ptNqa");
-        for (let p of children) {
-            console.log(p);
-            console.log(p.childNodes[0]);
-            if (p.childNodes.length == 1) {
-                continue;
-            }
-            let mph = parseFloat(p.childNodes[0].textContent);
-            if (!mph) {
-                continue;
-            }
-            let kmph = (mph * 1.609344).toFixed(2);
-            p.textContent = p.textContent + " / " + kmph + " kmph";
-        }
-        children = root.querySelectorAll(".data-point");
-        for (let p of children) {
-            console.log(p);
-            console.log(p.childNodes[0]);
-            if (p.childNodes.length != 1) {
-                continue;
-            }
-            let m = p.childNodes[0].textContent.match(/^([\d.]+) mph$/);
-            if (!m) {
-                continue;
-            }
-            let mph = parseFloat(m[1]);
-            if (!mph) {
-                continue;
-            }
-            let kmph = (mph * 1.609344).toFixed(2);
-            p.textContent = kmph + " kmph";
-        }
+  "use strict";
+  console.log("tampermonkey");
+  function replaceContent(root) {
+    let children = root.querySelectorAll(".ptNqa");
+    for (let p of children) {
+      console.log(p);
+      console.log(p.childNodes[0]);
+      if (p.childNodes.length == 1) {
+        continue;
+      }
+      let mph = parseFloat(p.childNodes[0].textContent);
+      if (!mph) {
+        continue;
+      }
+      let kmph = (mph * 1.609344).toFixed(2);
+      p.textContent = p.textContent + " / " + kmph + " kmph";
     }
-    replaceContent(document.body);
-    const observer = new MutationObserver((mutations) => {
-        for (let mutation of mutations) {
-            if (mutation.type != "childList") {
-                continue;
-            }
-            for (let node of mutation.addedNodes) {
-                if (node.nodeType != 1) {
-                    continue;
-                }
-                replaceContent(node);
-            }
+    children = root.querySelectorAll(".data-point");
+    for (let p of children) {
+      console.log(p);
+      console.log(p.childNodes[0]);
+      if (p.childNodes.length != 1) {
+        continue;
+      }
+      let m = p.childNodes[0].textContent.match(/^([\d.]+) mph$/);
+      if (!m) {
+        continue;
+      }
+      let mph = parseFloat(m[1]);
+      if (!mph) {
+        continue;
+      }
+      let kmph = (mph * 1.609344).toFixed(2);
+      p.textContent = kmph + " kmph";
+    }
+  }
+  replaceContent(document.body);
+  const observer = new MutationObserver((mutations) => {
+    for (let mutation of mutations) {
+      if (mutation.type != "childList") {
+        continue;
+      }
+      for (let node of mutation.addedNodes) {
+        if (node.nodeType != 1) {
+          continue;
         }
-    });
-    observer.observe(document.body, { subtree: true, childList: true });
+        replaceContent(node);
+      }
+    }
+  });
+  observer.observe(document.body, { subtree: true, childList: true });
 })();
