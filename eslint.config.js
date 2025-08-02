1581e1c1abaf8708f35d17fd9ce4d060c615c4b2
Yongxin Wang <fefe82@qq.com>
2025-08-02 13:14:38 +0800

    connet db

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from "eslint/config";
-
+import js from "@eslint/js";
+import globals from "globals";
 export default defineConfig([
   {
+    languageOptions: { globals: { ...globals.greasemonkey, ...globals.browser } },
+    plugins: {
+      js,
+    },
+    extends: ["js/recommended"],
     rules: {
       semi: "error",
       "prefer-const": "error",
