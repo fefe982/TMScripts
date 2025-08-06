74278f18a44dc21819fb52fdebfc91345c4e1627
Yongxin Wang <fefe82@qq.com>
2025-08-07 07:11:31 +0800

    start wta

--- wtt.js
+++ wtt.js
@@ -8,6 +8,7 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=worldtabletennis.com
 // @grant        unsafeWindow
 // @grant        GM_xmlhttpRequest
+// @connect      localhost
 // ==/UserScript==
 
 (async function () {
