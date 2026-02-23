4227be9160339704eb7f6c97972b84e145b5d77a
Yongxin Wang <fefe82@qq.com>
2026-02-24 05:53:10 +0800

    site update

--- mlb.js
+++ mlb.js
@@ -14,67 +14,106 @@
 (function () {
   "use strict";
   console.log("tampermonkey");
-  let mph_to_kmph = (mph) => (mph * 1.609344).toFixed(2);
-  let mph_to_mps = (mph) => (mph * 0.447037).toFixed(1);
+  const mph_to_kmph = (/** @type {number} */ mph) => (mph * 1.609344).toFixed(2);
+  const mph_to_mps = (/** @type {number} */ mph) => (mph * 0.447037).toFixed(1);
+  const ft_to_m = (/** @type {number} */ ft) => (ft * 0.3048).toFixed(1);
+  /**
+   * @param {HTMLElement} root
+   */
   function replaceBallSpeed(root) {
-    let children = root.querySelectorAll(".ptNqa");
-    for (let p of children) {
-      console.log(p);
+    const children = root.querySelectorAll(".fcPMBB, .iacVON");
+    for (const p of children) {
+      console.log(p, p.childNodes.length);
       console.log(p.childNodes[0]);
-      if (p.childNodes.length == 1) {
+      if (p.childNodes.length != 2) {
         continue;
       }
-      let mph = parseFloat(p.childNodes[0].textContent);
-      if (!mph) {
+      const src = Number.parseFloat(p.childNodes[0].textContent || "");
+      if (!src) {
         continue;
       }
-      let kmph = mph_to_kmph(mph);
-      p.textContent = p.textContent + " / " + kmph + " kmph";
+      if (p.childNodes[1].textContent?.endsWith("mph")) {
+        const kmph = mph_to_kmph(src);
+        const new_unit = p.childNodes[1].cloneNode(true);
+        if (new_unit instanceof HTMLElement) {
+          new_unit.textContent = "kmph";
+        } else {
+          new_unit.textContent = " kmph";
+        }
+        p.appendChild(document.createTextNode(" / "));
+        p.appendChild(document.createTextNode(kmph));
+        p.appendChild(new_unit);
+      }
+      if (p.childNodes[1].textContent?.endsWith("ft")) {
+        const m = ft_to_m(src);
+        const new_unit = p.childNodes[1].cloneNode(true);
+        new_unit.textContent = "m";
+        if (new_unit instanceof HTMLElement) {
+          new_unit.textContent = "m";
+        } else {
+          new_unit.textContent = " m";
+        }
+        p.appendChild(document.createTextNode(" / "));
+        p.appendChild(document.createTextNode(m));
+        p.appendChild(new_unit);
+      }
     }
   }
+  /**
+   * @param {HTMLElement} root
+   */
   function replacePitchSpeed(root) {
-    let children = root.querySelectorAll(".data-point");
-    for (let p of children) {
+    const children = root.querySelectorAll(".data-point");
+    for (const p of children) {
       if (p.childNodes.length != 1) {
         continue;
       }
-      let m = p.childNodes[0].textContent.match(/^([\d.]+) mph$/);
+      const m = p.childNodes[0].textContent?.match(/^([\d.]+) mph$/);
       if (!m) {
         continue;
       }
-      let mph = parseFloat(m[1]);
+      const mph = Number.parseFloat(m[1]);
       if (!mph) {
         continue;
       }
-      let kmph = mph_to_kmph(mph);
+      const kmph = mph_to_kmph(mph);
       p.textContent = kmph + " kmph";
     }
   }
-  function replace_fazIkQ(p) {
+  /**
+   * @param {HTMLElement} p
+   */
+  function replace_overall_list(p) {
     if (p.childNodes[0].textContent == "Weather") {
-      let m = p.childNodes[2].textContent.match(/^(\d+) degrees(.*)$/);
+      const m = new RegExp(/^(\d+) degrees(.*)$/).exec(p.childNodes[2].textContent || "");
       if (!m) {
         return;
       }
-      let deg = ((parseInt(m[1]) - 32) / 1.8).toFixed(1);
+      const deg = ((Number.parseInt(m[1]) - 32) / 1.8).toFixed(1);
       p.childNodes[2].textContent = deg + " ℃" + m[2];
     }
     if (p.childNodes[0].textContent == "Wind") {
-      let m = p.childNodes[2].textContent.match(/^(\d+) mph(.*)$/);
+      const m = new RegExp(/^(\d+) mph(.*)$/).exec(p.childNodes[2].textContent || "");
       if (!m) {
         return;
       }
-      let mph = parseFloat(m[1]);
-      let mps = mph_to_mps(mph);
+      const mph = Number.parseFloat(m[1]);
+      const mps = mph_to_mps(mph);
       p.childNodes[2].textContent = mps + " m/s" + m[2];
     }
   }
+  /**
+   * @param {HTMLElement} root
+   */
   function replaceEnvironment(root) {
-    let children = root.querySelectorAll(".fazIkQ");
-    for (let p of children) {
-      replace_fazIkQ(p);
+    const children = root.querySelectorAll(".cwAX");
+    for (const p of children) {
+      replace_overall_list(/** @type {HTMLElement} */ (p));
     }
   }
+  /**
+   * @param {HTMLElement} root
+   */
   function replaceContent(root) {
     replaceBallSpeed(root);
     replacePitchSpeed(root);
@@ -82,18 +121,21 @@
   }
   replaceContent(document.body);
   const observer = new MutationObserver((mutations) => {
-    for (let mutation of mutations) {
+    for (const mutation of mutations) {
       if (mutation.type != "childList") {
-        if (mutation.target.parentElement.classList.contains("fazIkQ")) {
-          replace_fazIkQ(mutation.target.parentElement);
+        if (mutation.target.parentElement?.classList.contains("cwAX")) {
+          replace_overall_list(mutation.target.parentElement);
         }
         continue;
       }
-      for (let node of mutation.addedNodes) {
-        if (node.nodeType != 1) {
+      for (const node of mutation.addedNodes) {
+        if (node.nodeType !== 1) {
           continue;
         }
-        replaceContent(node);
+        // Only call replaceContent if node is an HTMLElement
+        if (node instanceof HTMLElement) {
+          replaceContent(node);
+        }
       }
     }
   });
